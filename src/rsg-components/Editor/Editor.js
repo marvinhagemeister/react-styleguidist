54a5564ba60d71144be9ee01886675ea8d3e66c2
Marvin Hagemeister <marvin.hagemeister@sevenval.com>
2016-10-20 17:06:59 +0200

    Remove codemirror editor
    This saves about 3.5mb in bundle size

--- src/rsg-components/Editor/Editor.js
+++ src/rsg-components/Editor/Editor.js
@@ -1,63 +1,19 @@
-// CodeMirror
-import 'codemirror/mode/jsx/jsx';
-import 'codemirror/lib/codemirror.css';
-
-import React, { Component, PropTypes } from 'react';
-import debounce from 'lodash/debounce';
-import Codemirror from 'react-codemirror';
+import React, { PropTypes } from 'react';
 import EditorRenderer from 'rsg-components/Editor/EditorRenderer';
 
-const codemirrorOptions = {
-	mode: 'jsx',
-	lineNumbers: false,
-	lineWrapping: true,
-	smartIndent: false,
-	matchBrackets: true,
-	viewportMargin: Infinity,
+const Editor = ({ code }) => {
+	return (
+		<EditorRenderer>
+			<div className="CodeMirror">
+				<div className="CodeMirror-scroll">
+					<pre className="CodeMirror-lines">{code}</pre>
+				</div>
+			</div>
+		</EditorRenderer>);
 };
 
-const cssRequire = require.context('codemirror/theme/', false, /^\.\/.*\.css$/);
+Editor.propTypes = {
+	code: PropTypes.string.isRequired,
+};
 
-const UPDATE_DELAY = 10;
-
-export default class Editor extends Component {
-	static propTypes = {
-		code: PropTypes.string.isRequired,
-		onChange: PropTypes.func.isRequired,
-	};
-	static contextTypes = {
-		config: PropTypes.object.isRequired,
-	};
-
-	constructor() {
-		super();
-		this.handleChange = debounce(this.handleChange.bind(this), UPDATE_DELAY);
-	}
-
-	componentWillMount() {
-		const { highlightTheme } = this.context.config;
-		cssRequire(`./${highlightTheme}.css`);
-	}
-
-	shouldComponentUpdate() {
-		return false;
-	}
-
-	handleChange(newCode) {
-		this.props.onChange(newCode);
-	}
-
-	render() {
-		const { code } = this.props;
-		const { highlightTheme } = this.context.config;
-		const options = {
-			...codemirrorOptions,
-			theme: highlightTheme,
-		};
-		return (
-			<EditorRenderer>
-				<Codemirror value={code} onChange={this.handleChange} options={options} />
-			</EditorRenderer>
-		);
-	}
-}
+export default Editor;
